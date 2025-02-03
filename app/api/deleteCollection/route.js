502a1d2456bb59410a33f924f6394de989e3c91a
Ankit Singh Chandel <ankitlucky1016@gmail.com>
2025-02-03 10:57:44 +0530

    deletion correct also 5 min refresh

--- app/api/deleteCollection/route.js
+++ app/api/deleteCollection/route.js
@@ -1,32 +1,28 @@
 import { MongoClient } from 'mongodb';
 
 // MongoDB connection string
-const url = process.env.MONGO_DB_URI; // Use your MongoDB connection string
+const url = process.env.MONGO_DB_URI;
 const dbName = 'promptSearch';
 
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const client = new MongoClient(url);
+export async function GET(req) {
+  const client = new MongoClient(url);
 
-    try {
-      await client.connect();
-      const db = client.db(dbName);
+  try {
+    await client.connect();
+    const db = client.db(dbName);
 
-      // Specify the collection to delete
-      const collectionName = 'news';
+    // Specify the collection to delete
+    const collectionName = 'news';
 
-      // Drop the collection
-      await db.collection(collectionName).drop();
-      console.log(`Collection ${collectionName} has been deleted.`);
-      
-      res.status(200).json({ message: 'Collection deleted successfully' });
-    } catch (error) {
-      console.error('Error deleting collection:', error);
-      res.status(500).json({ error: 'Failed to delete collection' });
-    } finally {
-      await client.close();
-    }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+    // Drop the collection
+    await db.collection(collectionName).drop();
+    console.log(`Collection ${collectionName} has been deleted.`);
+
+    return Response.json({ message: 'Collection deleted successfully' }, { status: 200 });
+  } catch (error) {
+    console.error('Error deleting collection:', error);
+    return Response.json({ error: 'Failed to delete collection' }, { status: 500 });
+  } finally {
+    await client.close();
   }
 }
