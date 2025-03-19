9eec9fa01db29a47f83a074c556ca6be3cbda255
Ankit Singh Chandel <ankitlucky1016@gmail.com>
2025-03-19 11:14:23 +0530

    comment box design changed

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,12 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
         <h1 className='head_text text-center'>
-            Find and share 
+        
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+      <span className='orange_gradient text-center'> Timely News,Always Relevant.</span>
     </h1>
     <p className='desc text-center'>
-      Promptsearch provides community of people who share creative and useful prompts and you can join us too !!
+    Providing Insightful and Timely News to Keep You Informed and Ahead.
     </p>
 
     <Feed/>
